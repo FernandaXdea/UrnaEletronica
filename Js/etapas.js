0f475ed6e8bd9fd99c2f0987b8ee5d88a4968a78
FernandaXdea <nandaxdea@gmail.com>
2022-08-21 13:11:50 -0300

    ajustandojs

---
+++ Js/etapas.js
@@ -0,0 +1,72 @@
+let etapas= [ 
+    {
+        titulo: 'DEPUTADO',
+        numeros: 5,
+        candidatos: [{
+    numero: 77111,
+    nome: 'Charles Deluvio',
+    partido: 'PA',
+    fotos: [
+        {url:'charlesdeluvio.jpg' , legenda:'Deputado'}
+    ]
+ },
+ {
+    numero: 84001,
+    nome: 'Kelvin Valerio',
+    partido: 'PRE',
+    fotos: [
+        {url:'KelvinValerio.jpg' , legenda:'Deputado'}
+    ]
+ },
+ {
+    numero: 83001,
+    nome: 'Victor Grabarczyk',
+    partido: 'PJM',
+    fotos: [
+        {url:'UrnaEletronica/IMG/victor-grabarczyk.jpg' , legenda:'Deputado'}
+    ]
+ },
+ {
+    numero: 90111,
+    nome: 'Karsten Winegeart',
+    partido: 'PFR',
+    fotos: [
+        {url:'UrnaEletronica/IMG/karsten-winegeart.jpg' , legenda:'Deputado'}
+    ]
+ },
+ {
+    numero: 95001,
+    nome: 'Geovana Souza',
+    partido: 'PLJ',
+    fotos: [
+        {url:'UrnaEletronica/IMG/GeovanaSouza.jpg' , legenda:'Deputado'}
+    ]
+ },
+ {
+    numero: 99001,
+    nome: 'Rei Dellas',
+    partido: 'PKZ',
+    fotos: [
+        {url:'UrnaEletronica/IMG/Rei-Dellas.jpg' , legenda:'Deputado'}
+    ]
+ },
+]},
+{
+    titulo: 'PRESIDENTE',
+    numeros: 2,
+    candidatos: [
+        {
+            numeri: '17',
+            nome: 'Bozo',
+            partido: 'PNC',
+            vice: 'Bozo Filho',
+            fotos: [
+                { url: 'bozo.jpg', legenda: 'Presidente', small: false },
+                { url: 'bozo_Filho.jpg', legenda: 'Vice', small: true },
+            ]
+        },
+]}
+
+
+
+]
